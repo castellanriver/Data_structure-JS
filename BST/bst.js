1414272f3f91442b17a9c034caadd5570a1c5e01
JeremyKang <castleownerkang@gmail.com>
2023-06-27 21:59:16 +0900

    b tree method added

--- BST/bst.js
+++ BST/bst.js
@@ -15,27 +15,61 @@ class BinarySearchTree {
     if(this.root === null) {
       this.root = newNode;
       return this;
-    } else {
-      var current = this.root;
-      while(true) {
-        if(value === current.value) return undefined;
-        if(value < current.value) {
-          if(current.left === null) {
-            current.left = newNode;
-            return this;
-          } else {
-            current = current.left;
-          }
-        } else if(value > current.value) {
-          if(current.right === null) {
-            current.right = newNode;
-            return this;
-          } else {
-            current = current.right
-          }
-        }
+    } 
+    var current = this.root;
+    while(true) {
+      if(value === current.value) return undefined;
+      if(value < current.value) {
+        if(current.left === null) {
+          current.left = newNode;
+          return this;
+        } 
+        current = current.left;          
+      } else if(value > current.value) {
+        if(current.right === null) {
+          current.right = newNode;
+          return this;
+        } 
+        current = current.right          
       }
     }
+  }
+
+  // output => node
+
+  // find(value) {
+  //   if(this.root === null) return false;
+  //   var current = this.root;
+  //       found = false;
+  //   while(current && !found) {
+  //     if(value < current.value) {
+  //       current = current.left;
+  //     } else if (value > current.value) {
+  //       current = current.right;
+  //     } else {
+  //       found = true;
+  //     }
+  //   }
+  //   if(!found) return undefined;  
+  //   return current;  
+  // }
+
+  // output => T or F
+
+  contains(value) {
+    if(this.root === null) return false;
+    var current = this.root;
+        found = false;
+    while(current && !found) {
+      if(value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;  
   }
 }
 
